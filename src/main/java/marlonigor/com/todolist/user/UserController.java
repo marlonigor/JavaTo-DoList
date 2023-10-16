3e7615237c1081dcd6580cd77a4b5e30926d06c0
marlonigor <igormarlon.o@gmail.com>
2023-10-15 22:52:34 -0300

    bcrypting user passwords

--- src/main/java/marlonigor/com/todolist/user/UserController.java
+++ src/main/java/marlonigor/com/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 
 @RestController
 @RequestMapping("/users")
@@ -25,6 +27,11 @@ public class UserController {
             //Mensagem de erro + Status code
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+
+        int cost;
+        var passwordHashred = BCrypt.withDefaults().hashToString(cost=12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
